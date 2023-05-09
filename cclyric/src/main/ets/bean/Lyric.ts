f7728be4be6c8cb71458b38b0e7eff4df4332920
seagazer <seagazer@qq.com>
2023-05-09 10:39:11 +0800

    [dev]更新配置信息，删除本地lyric组件代码改为har依赖

--- cclyric/src/main/ets/bean/Lyric.ts
+++
@@ -1,9 +0,0 @@
-export class Lyric {
-    readonly timestamp: number
-    readonly text: string
-
-    constructor(timestamp: number, text: string) {
-        this.timestamp = timestamp
-        this.text = text
-    }
-}
--- cclyric/src/main/ets/parse/IParser.ts
+++
@@ -1,7 +0,0 @@
-import { Lyric } from '../bean/Lyric';
-
-export interface IParser {
-
-    parse(source: any): Array<Lyric>
-
-}
