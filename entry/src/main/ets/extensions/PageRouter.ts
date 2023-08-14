caa95e14dc6deb7273fbbc7e44863e4f71062f99
seagazer <seagazer@qq.com>
2023-08-14 22:18:56 +0800

    [dev]增加更多菜单选项

--- entry/src/main/ets/extensions/PageRouter.ts
+++ entry/src/main/ets/extensions/PageRouter.ts
@@ -6,6 +6,8 @@ export enum PageRouter {
     PAGE_SPLASH = "splash/PageSplash",
     PAGE_GUIDE_WELCOME = "guide/PageWelcome",
     PAGE_GUIDE_SETUP = "guide/PageSetup",
-    PAGE_HOME = "guide/PageFileList",
+    PAGE_GUIDE_BROWSE = "guide/PageFileList",
     PAGE_PLAY = "home/PageHome",
+    PAGE_SETTINGS = "settings/PageSettings",
+    PAGE_MEDIA_LIBRARY = "medialibrary/PageMediaLibrary",
 }
