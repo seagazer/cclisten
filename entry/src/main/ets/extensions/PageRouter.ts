6593df07b7e55e6018e456966c5058098b1a967e
seagazer <seagazer@qq.com>
2023-09-11 23:25:27 +0800

    [dev]增加自定义倒计时关闭设置

--- entry/src/main/ets/extensions/PageRouter.ts
+++ entry/src/main/ets/extensions/PageRouter.ts
@@ -5,7 +5,7 @@
 export enum PageRouter {
     PAGE_SPLASH = "splash/PageSplash",
     PAGE_GUIDE_WELCOME = "guide/PageWelcome",
-    PAGE_GUIDE_SETUP = "guide/PageSetup",
+    PAGE_GUIDE_SETUP = "guide/PageTheme",
     PAGE_GUIDE_BROWSE = "guide/PageFileList",
     PAGE_PLAY = "home/PageHome",
     PAGE_SETTINGS = "settings/PageSettings",
